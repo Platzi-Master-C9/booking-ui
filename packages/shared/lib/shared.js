eddf79c34deb9899764c0bbba840231003fa390a
taberoajorge <taberoajorge@pm.me>
2022-03-05 01:42:47 -0600

    chore(structure): update structure

--- packages/shared/lib/shared.js
+++ packages/shared/lib/shared.js
@@ -1,7 +1,10 @@
 'use strict';
 
-module.exports = shared;
+const { Button } = require("./components/Button");
+
+
+module.exports = Button;
 
 function shared() {
-    // TODO
+    console.log('epale');
 }
