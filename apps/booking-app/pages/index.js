8f32dd4314be7521a4dfbc6dd593f4b2e25d6335
jlonuz <lonuz.j@gmail.com>
2022-03-09 01:11:19 -0300

    add react testing library dependencie

--- apps/booking-app/pages/index.js
+++ apps/booking-app/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-//import Button from '@booking-ui/shared'
+import { Button } from '@booking-ui/shared'
 
 export default function Home() {
   return (
@@ -16,7 +16,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-        {/* <Button /> */}
+        <Button />
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
