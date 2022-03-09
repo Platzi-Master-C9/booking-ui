a30dc168a0e31ca791c66ff28b09f1252f0463ce
Jorge Alexander Taberoa Jimenez <65203661+taberoajorge@users.noreply.github.com>
2022-03-09 13:04:14 -0600

    Create Next.js Booking-app (#53)
    * chore(structure): create root monorepo
    
    * chore(structure): update structure
    
    * chore(structure): resolve connections
    
    * feat(structure): add prettier config, and update dependencies
    
    * feat(structure): update source in package.json
    
    * feat(structure): update source in package.json
    
    * fix(structure): add app folder
    
    * Update .gitignore
    
    * fix(structure): add app folder
    
    * fix(structure): add app folder
    
    * fix(structure): add app folder
    
    * fix(structure): add structure to all packages
    
    * create next js app
    
    * add react testing library dependencie
    
    * feat(structure): creating necx.js app with testing, storybooks
    
    Co-authored-by: jlonuz <lonuz.j@gmail.com>

---
+++ packages/search-engine/src/components/helloWorld.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const SearchEngine = () => {
+  return (
+    console.log('Hello from SearchEngine')
+    // <h1>Hello from SearchEngine</h1>
+  );
+};
---
+++ packages/shared/src/index.js
@@ -0,0 +1,6 @@
+export { Button } from './components/Button';
+export { Geolocation } from '@booking-ui/geolocation';
+export { SearchEngine } from '@booking-ui/search-engine';
+export { MessagingSystem } from '@booking-ui/messaging-system';
+
+
