24ea64bc82e4e94ee8bde86fe79520e5e523fce1
Pedro Silva <pedroalejsp@gmail.com>
2022-03-16 12:09:57 -0300

    Rendering the admin-panel component UI demo

--- packages/admin-panel/src/components/helloWorld.js
+++ packages/admin-panel/src/components/helloWorld.js
@@ -3,6 +3,6 @@ import React from 'react';
 export const AdminPanel = () => {
   return (
     //console.log('hello from admin panel')
-    <h1>hello from admin panel :D</h1>
+    <h1>Hello from admin panel :D</h1>
   );
 };
