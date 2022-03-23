e28c38ec5dc47cb368065575d9e8958dbcddbd4b
Cristhian-Medina <criscis.mecasa@gmail.com>
2022-03-22 19:29:21 -0500

    Layout the search results page #41
    * feat: layout the results page structure

---
+++ packages/search-engine/src/components/resultsPage/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="footer__container">
+      <div className="footer__wrapper">
+        <a className='link' href='./'>Sobre Nosotros</a>
+        <a className='link' href='./'>Propósito del proyecto</a>
+        <a className='link' href='./'>Redes Sociales</a>
+      </div>
+    </footer>
+  )
+}
---
+++ packages/search-engine/src/components/resultsPage/Map.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Map() {
+  return <div className="map__container"><img/></div>
+}
