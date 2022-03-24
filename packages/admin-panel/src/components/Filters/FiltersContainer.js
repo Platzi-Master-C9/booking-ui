961e47b2d37149aa2e5e085e0d0d4dabed6a6e7e
MaidedPonce <maided.h.p90@gmail.com>
2022-03-23 20:42:13 -0600

    adding responsive filters

--- packages/admin-panel/src/components/Filters/FiltersContainer.js
+++ packages/admin-panel/src/components/Filters/FiltersContainer.js
@@ -81,6 +81,7 @@ export const FiltersContainer = () => {
 
     return (
         <>
+            <button className='buttonFilter' onClick={() => setOpenFilters(true)}>Filtrar</button>
             <div className='containerFilters'>
                 <div className="containerCards">
                     <CardContainer type={"Tipo"} titleOne="Anfitrión" titleTwo="Huésped" descriptionOne="Descripcion para anfitrion" descriptionTwo="Descripcion para huesped" show={show.showOne} checkedOne={checked.checkedOne} checkedTwo={checked.checkedTwo} handleSetShow={() => handleSetShow("showOne", !show.showOne)} handleChexboxOne={(e) => handleSetChecked("checkedOne", e.target.checked)} handleChexboxTwo={(e) => handleSetChecked("checkedTwo", e.target.checked)} handleChexboxOneDelete={(e) => handleSetDelete("checkedOne", "checkedTwo", false, false)} />
@@ -109,10 +110,14 @@ export const FiltersContainer = () => {
                         )))
                 }
             </div>
-            <button onClick={() => setOpenFilters(true)}>Filtrar</button>
             {
                 openFilters && <div id='filter' className='modal'>
                     <div className='containerFiltersTwo'>
+                        <figure onClick={() => setOpenFilters(false)} className='containerEq'>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="eq" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
+                        </svg>
+                        </figure>
                         <div className="containerCards">
                             <CardContainer type={"Tipo"} titleOne="Anfitrión" titleTwo="Huésped" descriptionOne="Descripcion para anfitrion" descriptionTwo="Descripcion para huesped" show={show.showOne} checkedOne={checked.checkedOne} checkedTwo={checked.checkedTwo} handleSetShow={() => handleSetShow("showOne", !show.showOne)} handleChexboxOne={(e) => handleSetChecked("checkedOne", e.target.checked)} handleChexboxTwo={(e) => handleSetChecked("checkedTwo", e.target.checked)} handleChexboxOneDelete={(e) => handleSetDelete("checkedOne", "checkedTwo", false, false)} />
                             <CardContainer type={"Estado"} titleOne="Activo" titleTwo="Inactivo" descriptionOne="Descripcion para activo" descriptionTwo="Descripcion para inactivo" show={show.showTwo} checkedOne={checked.checkedThree} checkedTwo={checked.checkedFour} handleSetShow={() => handleSetShow("showTwo", !show.showTwo)} handleChexboxOne={(e) => handleSetChecked("checkedThree", e.target.checked)} handleChexboxTwo={(e) => handleSetChecked("checkedFour", e.target.checked)} handleChexboxOneDelete={(e) => handleSetDelete("checkedThree", "checkedFour", false, false)} />
