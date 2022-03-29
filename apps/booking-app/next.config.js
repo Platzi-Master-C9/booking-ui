6dab5a7fc801e983a213d8d685f8dd7102c34ee3
Angel <74243893+701i@users.noreply.github.com>
2022-03-29 11:55:28 -0600

    Created the Places package and configurations (#91)
    * Created the Places package
    
    * render Places package
    
    * config. of Places package
    
    * eslint config
    
    * jest config
    
    * storybook config
    
    * added EOF
    
    * added EOF
    
    * added EOF
    
    * added EOF
    
    * added EOF
    
    * added EOF
    
    * added EOF
    
    * added EOF
    
    * added EOF
    
    * added EOF
    
    * added EOF
    
    * added the config. of SASS

--- apps/booking-app/next.config.js
+++ apps/booking-app/next.config.js
@@ -1,6 +1,7 @@
 const withPlugins = require('next-compose-plugins');
 const withTM = require('next-transpile-modules')([
   '@booking-ui/shared',
+  '@booking-ui/places',
   '@booking-ui/search-engine',
   '@booking-ui/messaging-system',
   '@booking-ui/geolocation',
