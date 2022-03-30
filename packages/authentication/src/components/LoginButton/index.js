86be8924f212accdd8ccfb9e2e175fa0bd88f006
Cristian Diaz <crisdi384@hotmail.com>
2022-03-30 11:32:27 -0500

    feat: instalacion de sass a booking-app

--- packages/authentication/src/components/LoginButton/index.js
+++ packages/authentication/src/components/LoginButton/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import css from './LoginButton.module.scss';
 
 export const LoginButton = ({ label = 'Login' }) => {
-  return <button className={css['login-button']}>{label}</button>;
+  return <button className={css.loginButton}>{label}</button>;
 };
