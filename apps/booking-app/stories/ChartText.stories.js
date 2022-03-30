c61fb375c0fcfd94cbfa9a85e1fe643bae8ccfcc
Arath Astorga <xarselt@gmail.com>
2022-03-30 13:39:03 -0600

    chore: fix typos, export component on storybook and change img for a base64 string

--- apps/booking-app/stories/ChartText.stories.js
+++
@@ -1,8 +0,0 @@
-import ChartText from "../../../packages/messaging-system/src/components/ChartText/ChartText";
-
-export default {
-    title: "ChartText",
-    component: ChartText,
-}
-
-export const SendTextFirst = () => <ChartText />
