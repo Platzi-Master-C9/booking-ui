ab24ac6d431ba2e8fffb4f270120da8b67896d12
EdwinSanFi <jaret-@live.com.mx>
2022-03-31 18:03:12 -0600

    feat(Create the filter of the calendar filter): Add the calendar filter to the admin section

--- packages/shared/src/index.js
+++ packages/shared/src/index.js
@@ -5,3 +5,4 @@ export { MessagingSystem } from '@booking-ui/messaging-system';
 export { Demo, ResultsPage } from '@booking-ui/search-engine';
 export * from '@booking-ui/authentication';
 export { AdminPanel, DropdownMenu } from '@booking-ui/admin-panel';
+export { Calendar } from '@booking-ui/calendar';
