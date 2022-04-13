5be11df9eb216873fb0c098327b5d541d976b027
Nahomi <nahyomi2000@gmail.com>
2022-04-13 17:18:27 -0500

    Created component ResposeTime with modules and SASS

--- packages/shared/src/index.js
+++ packages/shared/src/index.js
@@ -1,8 +1,7 @@
 export { Button } from './components/Button';
 export { Places } from '@booking-ui/places';
 export { Geolocation } from '@booking-ui/geolocation';
-export { MessagingSystem, TextBox } from '@booking-ui/messaging-system';
-export { Demo } from '@booking-ui/search-engine';
+export { MessagingSystem, TextBox, ResponseTime } from '@booking-ui/messaging-system';
 export { UserAccount } from '@booking-ui/user-account';
 export { Demo, ResultsPage } from '@booking-ui/search-engine';
 export * from '@booking-ui/authentication';
