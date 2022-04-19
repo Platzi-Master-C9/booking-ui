7d82eeeceb01726f0cd7a2f8606c3954b24c1ea8
JESUS CARLOS CARDENAS <jccp33@gmail.com>
2022-04-18 22:22:31 -0700

    Add login form v3

--- apps/booking-app/pages/admin/index.js
+++ apps/booking-app/pages/admin/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { LoginContainer } from '../../../../packages/admin-panel/src/components/LoginContainer'
 
+const isLogged = false;
+
 const Admin = () => {
-  return (
-    <LoginContainer />
-  );
+  if(isLogged){
+    return (
+      <>Metric</>
+    );
+  }else{
+    return (
+      <LoginContainer />
+    );
+  }
 };
 
 export default Admin;
