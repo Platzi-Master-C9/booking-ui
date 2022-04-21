b91fe2e8d17a3c53c39ea86e3ff78ff21f7e6006
juli666n <julianhfan@gmail.com>
2022-04-20 19:06:53 -0500

    routing added to the favorite cards in the favorites view

--- packages/user-account/src/components/favoriteView/layout.js
+++ packages/user-account/src/components/favoriteView/layout.js
@@ -2,8 +2,11 @@ import React from 'react';
 import "./assets/styles/style.css";
 import Footer from '../footer';
 import Header from '../header'
+import Link from 'next/link'
 import { FavoriteCard } from '../favoriteCard/favoriteItemCard';
 
+const ROUTE_POST_ID = "favorite/[id]";
+const  goToFavorite = () => {}
 const favorites = [
   {id: '1', cardPhoto: 'https://cdn2.infocasas.com.uy/repo/img/55835182a525b55835182a52d1.jpg.th810.jpg', cardTitle: 'Lujosa suite en ciudad bolivar'},
   {id: '2', cardPhoto: 'https://directorioicc.gov.co/sites/default/files/espacio-audiovisual-barrio-los-laches/481_barrio_los_laches036.jpg', cardTitle: 'suite en los laches'},
@@ -15,10 +18,17 @@ export const FavoritesView = () => {
   return (
     <div className="container"> 
       <Header />
-      <h2>Mis favoritos</h2>
+      <h2 className='title'>Mis favoritos</h2>
       <div className="favorite-grid">
         {favorites.map(favorite =>(
-          <FavoriteCard key={favorite.id} cardPhoto={favorite.cardPhoto} cardTitle={favorite.cardTitle}/>
+          <Link href={{
+            pathname: ROUTE_POST_ID,
+            query: { id: favorite.id }
+          }}>
+            <a onClick={goToFavorite}>
+            <FavoriteCard key={favorite.id} cardPhoto={favorite.cardPhoto} cardTitle={favorite.cardTitle}/>
+            </a>
+          </Link>
         )) }
       </div>
     <Footer />
