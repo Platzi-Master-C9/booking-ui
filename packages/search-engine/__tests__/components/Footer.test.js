91d442a97e0ba60f82242f0d55d0632ed27ed3e7
Cristhian-Medina <criscis.mecasa@gmail.com>
2022-04-21 10:26:46 -0500

    feat: add test for resultsPage component

--- packages/search-engine/__tests__/components/Footer.test.js
+++ packages/search-engine/__tests__/components/Footer.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import { create } from 'react-test-renderer';
 
 import Footer from '../../src/components/Footer';
 
@@ -16,12 +15,5 @@ describe('<Footer />', () => {
     expect(footer.find('.link').at(0).text()).toEqual('About us');
     expect(footer.find('.link').at(1).text()).toEqual('Aim of the project');
     expect(footer.find('.link').at(2).text()).toEqual('Social media');
-  });
-});
-
-describe('Footer Snapshot', () => {
-  it('compounding the UI of the component', () => {
-    const footer = create(<Footer />);
-    expect(footer.toJSON()).toMatchSnapshot();
   });
 });
