bb6de7244f6c59b898e1ca0e05a54c9dff356f33
Pedro Silva <pedroalejsp@gmail.com>
2022-04-24 21:25:55 -0400

    Fixed rebase conflicts

--- packages/admin-panel/src/components/Sidebar.jsx
+++ packages/admin-panel/src/components/Sidebar.jsx
@@ -1,20 +1,20 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import '../assets/styles/Sidebar.scss';
-import { SidebarData } from './SidebarData.js';
-import MenuIcon from '@mui/icons-material/Menu';
+import React, { useState } from "react";
+import styled from "styled-components";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import "../assets/styles/Sidebar.scss";
+import { SidebarData } from "./SidebarData.js";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const SidebarNav = styled.nav`
   position: fixed;
-  left: ${({ sidebar }) => (sidebar ? '0' : '-100%')};
+  left: ${({ sidebar }) => (sidebar ? "0" : "-100%")};
   transition: 350ms;
   z-index: 10;
 
   @media screen and (max-width: 412px) {
     & {
-      left: ${({ sidebar }) => (sidebar ? '-100%' : '0')};
+      left: ${({ sidebar }) => (sidebar ? "-100%" : "0")};
     }
   }
 
@@ -42,27 +42,11 @@ const TitleSidebar = styled.div`
   }
 `;
 
-<<<<<<< HEAD
-export const Sidebar = () => {
-  const [sidebar, setSidebar] = useState(true);
-  const showSidebar = () => setSidebar(!sidebar);
+export const Sidebar = ({ sidebar }) => {
   const router = useRouter();
 
   return (
     <>
-      <div className="Header">
-        <div className="row" id="Menu-hamburger">
-          <div id="Sidebar-list-icon">
-            <MenuIcon onClick={showSidebar} />
-          </div>
-        </div>
-        {/* <div className="Circulo" id="BS">
-          <p>Circulo</p>
-        </div>
-        <div className="Circulo" id="Perfil">
-          <p>Perfil</p>
-        </div> */}
-      </div>
       <SidebarNav className="Sidebar" sidebar={sidebar}>
         <ul className="SidebarList">
           {SidebarData.map((val, key) => {
@@ -71,7 +55,7 @@ export const Sidebar = () => {
                 <li
                   key={key}
                   className="row"
-                  id={router.pathname === val.link ? 'active' : ''}
+                  id={router.pathname === val.link ? "active" : ""}
                 >
                   <div id="Sidebar-list-icon">{val.icon}</div>
                   <TitleSidebar id="Sidebar-list-title" className="title">
@@ -86,26 +70,3 @@ export const Sidebar = () => {
     </>
   );
 };
-=======
-export const Sidebar = ({ sidebar }) => (
-  <SidebarNav className="Sidebar" sidebar={sidebar}>
-    <ul className="SidebarList">
-      {SidebarData.map((val, key) => {
-        return (
-          <li
-            key={key}
-            className="row"
-            onClick={() => {
-              window.location.pathname = val.link;
-            }}
-            // id={window.location.pathname === val.link ? 'active' : ''}
-          >
-            <div id="Sidebar-list-icon">{val.icon}</div>
-            <div id="Sidebar-list-title">{val.title}</div>
-          </li>
-        );
-      })}
-    </ul>
-  </SidebarNav>
-);
->>>>>>> Reorganized the layout and integrated the sidebar menu
