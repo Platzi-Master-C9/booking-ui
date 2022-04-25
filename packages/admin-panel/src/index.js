f3d4f8c114a61fc6e8c7288e6496c8b70c008b03
Pedro Silva <pedroalejsp@gmail.com>
2022-04-25 09:59:37 -0400

    Integrated the layout with the grid, sidebar, and navbar in header

--- packages/admin-panel/src/index.js
+++ packages/admin-panel/src/index.js
@@ -4,5 +4,4 @@ export { Layout } from './components/Layout';
 export { Header } from './components/Layout';
 export { Sidebar } from './components/Sidebar';
 export { Dashboard } from './components/Dashboard';
-export { NavBar } from './components/NavBar';
 export { FilterContainer } from './components/Filters/FiltersContainer';
