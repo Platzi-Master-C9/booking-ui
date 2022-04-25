01b94ca0c90ee08a6c22db0e7cd9d40a2e39cb8d
Pedro Silva <pedroalejsp@gmail.com>
2022-04-24 21:45:15 -0400

    Rendered the layout in users route for admin panel

--- apps/booking-app/pages/admin/usuarios/index.js
+++ apps/booking-app/pages/admin/usuarios/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Layout, Dashboard } from '@booking-ui/shared';
+<<<<<<< HEAD
 import { ContextOfFilters } from '@booking-ui/admin-panel/src/components/Filters/FiltersContext/ContextFilters';
 const Users = () => (
   <Layout>
@@ -13,3 +14,13 @@ const Users = () => (
 
 export default Users;
 
+=======
+
+const Users = () => (
+  <Layout>
+    <Dashboard />
+  </Layout>
+);
+
+export default Users;
+>>>>>>> Rendered the layout in users route for admin panel
