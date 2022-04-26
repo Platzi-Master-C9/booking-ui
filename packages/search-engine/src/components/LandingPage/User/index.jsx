a4f37f68305a7be585fdffa3e1402ce339eb35cb
Cristhian-Medina <criscis.mecasa@gmail.com>
2022-04-26 00:20:53 -0500

    feat: add test for resultsPage, Footer and Header components

--- packages/search-engine/src/components/LandingPage/User/index.jsx
+++ packages/search-engine/src/components/LandingPage/User/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import './User.css';
 
 const User = ({ classUser = 'users_container' }) => {
@@ -7,32 +6,13 @@ const User = ({ classUser = 'users_container' }) => {
     classUser = classUser + ' users_container';
   }
 
-=======
-import Image from 'next/image';
-
-import userImage from '../assets/iconUser.png';
-import './User.css';
-
-const User = ({ classUser }) => {
-  if (classUser !== 'landing') {
-    classUser = 'users_container';
-  } else {
-    classUser = classUser + ' users_container';
-  }
->>>>>>> Fixed rebase conflicts
   return (
     <div className={classUser}>
       <div className="link host">
         <a href="/">Hazte anfitrión</a>
       </div>
 
-<<<<<<< HEAD
       <div className="user"></div>
-=======
-      <div className="user">
-        <Image src={userImage} />
-      </div>
->>>>>>> Fixed rebase conflicts
     </div>
   );
 };
