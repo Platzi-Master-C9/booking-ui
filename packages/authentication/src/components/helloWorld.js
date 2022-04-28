fc1c82c2d201d85178d76c6f0e0f3a5321e321d4
Sebastian.gc <sebastian.gc@globant.com>
2022-04-27 21:33:06 -0500

    Connect with booking-services auth API

--- packages/authentication/src/components/helloWorld.js
+++ packages/authentication/src/components/helloWorld.js
@@ -1,5 +1,23 @@
+import { Auth0Provider } from '@auth0/auth0-react';
 import React from 'react';
 
 export const Authentication = () => {
-  return <h1>Hello from Authentication</h1>;
+  const handleConectAuthServices = async () => {
+    const response = await fetch('http://localhost:3000/auth/hi');
+    const data = response.json();
+    console.log('>>>data', data);
+  };
+
+  return (
+    <Auth0Provider
+      domain="YOUR_DOMAIN"
+      clientId="YOUR_CLIENT_ID"
+      redirectUri="/"
+    >
+      <h1>Hello from Authentication</h1>
+      <button onClick={() => handleConectAuthServices()}>
+        Run service "auth/host"
+      </button>
+    </Auth0Provider>
+  );
 };
--- packages/shared/src/index.js
+++ packages/shared/src/index.js
@@ -1,9 +1,8 @@
-export { Button } from './components/Button';
-export { Places } from '@booking-ui/places';
+export { AdminPanel, DropdownMenu, Sidebar } from '@booking-ui/admin-panel';
+export * from '@booking-ui/authentication';
 export { Geolocation } from '@booking-ui/geolocation';
 export { MessagingSystem, TextBox } from '@booking-ui/messaging-system';
-export { Demo } from '@booking-ui/search-engine';
-export { UserAccount } from '@booking-ui/user-account';
+export { Places } from '@booking-ui/places';
 export { Demo, ResultsPage } from '@booking-ui/search-engine';
-export * from '@booking-ui/authentication';
-export { AdminPanel, DropdownMenu, Sidebar } from '@booking-ui/admin-panel';
+export { UserAccount } from '@booking-ui/user-account';
+export { Button } from './components/Button';
