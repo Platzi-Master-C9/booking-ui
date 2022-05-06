47ef027d56351b6e86480f3e6299571da75aab1a
juli666n <julianhfan@gmail.com>
2022-05-06 02:36:15 -0500

    conection between favorites page and css modules succeded

--- apps/booking-app/pages/favorites/[id]/index.js
+++ apps/booking-app/pages/favorites/[id]/index.js
@@ -1,8 +1,9 @@
 import { useRouter } from "next/router";
 import Header from "@booking-ui/user-account/src/components/header";
 import Footer from "@booking-ui/user-account/src/components/footer";
+import styles from  "../../../styles/Favorite.module.scss";
 
-export default function favorite() {
+export default function Favorite() {
   const router = useRouter()
   const title = router.query.title
   const photoUrl = router.query.photoUrl
@@ -13,7 +14,7 @@ export default function favorite() {
   return (
     <div>
       <Header />
-      <h2 className='title'>{title}</h2>
+      <h2 className={styles.paragraph}>{title}</h2>
       <img src={photoUrl}></img>
       <p>{favorites}</p>
       <Footer />
