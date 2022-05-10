73d263bc30d904bff822d2e69b2844220cace7ea
Emanuel Alvarado <emanuelalvaradog@gmail.com>
2022-05-09 21:59:38 -0500

    feat: Search restuls map testing [Geolocation]

--- packages/shared/src/index.js
+++ packages/shared/src/index.js
@@ -5,9 +5,4 @@ export { MessagingSystem } from '@booking-ui/messaging-system';
 export { Demo, ResultsPage } from '@booking-ui/search-engine';
 export { UserAccount } from '@booking-ui/user-account';
 export * from '@booking-ui/authentication';
-<<<<<<< HEAD
-export * from '@booking-ui/notification-system';
-export * from '@booking-ui/admin-panel';
-=======
 export { AdminPanel, DropdownMenu, Sidebar } from '@booking-ui/admin-panel';
->>>>>>> ab501de7f1ab635a50669bb716cce081a2142349
