dc1c62881a74ec73aa2e4216f308b7743160a017
Juan Manuel Calderon Robles <sayroback@gmail.com>
2022-05-10 22:19:10 -0500

    Dashboard of admin/administrators, redesign with cards, filters for admins and super admins, array sorted alphabetically. The dashboard responds to the width of the display.

--- apps/booking-app/pages/admin/administradores/index.js
+++ apps/booking-app/pages/admin/administradores/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
+import { Layout, LayoutAdministrators } from '@booking-ui/shared';
 
-import { Layout, Dashboard, LayoutAdministrators } from '@booking-ui/shared';
-import { ContextOfFilters } from '@booking-ui/admin-panel/src/components/Filters/FiltersContext/ContextFilters';
 const Administradores = () => {
-  const url = 'https://demo8248442.mockable.io/admins';
   return (
     <Layout>
-      <ContextOfFilters>
-        <LayoutAdministrators url={url} />
-      </ContextOfFilters>
+      <LayoutAdministrators />
     </Layout>
   );
 };
--- packages/admin-panel/src/components/LayoutAdministrators.jsx
+++ packages/admin-panel/src/components/LayoutAdministrators.jsx
@@ -1,130 +1,40 @@
 import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
-import { CardContainer } from "./Filters/CardContainer";
-import { GridItem } from "./GridItem";
-import styles from "../assets/styles/Dashboard.module.scss";
-import { ContextFilters } from "./Filters/FiltersContext/ContextFilters";
+import styled from "styled-components";
+import { CardAdmin } from "./CardAdmin";
+import "../assets/styles/LayoutAdministrators.scss";
 
-export const LayoutAdministrators = ({ url }) => {
-  const [users, setUsers] = useState([]);
-  const [error, setError] = useState(null);
-  const baseURL = url;
+const api_url =
+  "https://admin-panel-booking-services.herokuapp.com/admin-panel/admins";
 
-  const {
-    checked,
-    setChecked,
-    show,
-    openFilters,
-    setOpenFilters,
-    handleSetShow,
-    handleSetChecked,
-    handleSetDelete,
-    usersC,
-  } = useContext(ContextFilters);
+const DashboardAdmin = styled.div`
+  width: 100%;
+`;
 
-  useEffect(() => {
-    getUsers();
+export const LayoutAdministrators = () => {
+  const [admins, setAdmins] = useState([]);
+
+  useEffect(async () => {
+    const response = await axios(api_url);
+    setAdmins(response.data.result);
   }, []);
 
-  const getUsers = async () => {
-    try {
-      const response = await axios.get(baseURL);
-      setUsers(response.data);
-    } catch (error) {
-      setError(error);
+  admins.sort((a, b) => {
+    if (a.fullName < b.fullName) {
+      return -1;
     }
-  };
-
-  const filterUsers = users.filter(
-    (u) =>
-      (checked.checkedOne && u.type === "Anfitrión") ||
-      (checked.checkedTwo && u.type === "Administrador") ||
-      (checked.checkedThree && u.status === "Activo") ||
-      (checked.checkedFour && u.status === "Inactivo") ||
-      (checked.checkedFive && u.validate === "Sí") ||
-      (checked.checkedSixx && u.validate === "No")
-  );
+    if (a.fullName > b.fullName) {
+      return 1;
+    }
+    return 0;
+  });
 
   return (
-    <section className={styles.dashboard}>
-      <h2>Administradores</h2>
-      <div className={styles.filtersItem}>
-        <div style={{ gridColumn: "1/2" }}>
-          <span className={styles.spanName}>Nombre</span>
-        </div>
-        <div style={{ gridColumn: "2/3" }}>
-          <CardContainer
-            styles
-            type={"Tipo"}
-            titleOne="Anfitrión"
-            titleTwo="Administrador"
-            descriptionOne="Descripcion para anfitrion"
-            descriptionTwo="Descripcion para Administrador"
-            show={show.showOne}
-            checkedOne={checked.checkedOne}
-            checkedTwo={checked.checkedTwo}
-            handleSetShow={() => handleSetShow("showOne", !show.showOne)}
-            handleChexboxOne={(e) =>
-              handleSetChecked("checkedOne", e.target.checked)
-            }
-            handleChexboxTwo={(e) =>
-              handleSetChecked("checkedTwo", e.target.checked)
-            }
-            handleChexboxOneDelete={(e) =>
-              handleSetDelete("checkedOne", "checkedTwo", false, false)
-            }
-          />
-        </div>
-        <div style={{ gridColumn: "3/4" }}>
-          <CardContainer
-            type={"Estado"}
-            titleOne="Activo"
-            titleTwo="Inactivo"
-            descriptionOne="Descripcion para activo"
-            descriptionTwo="Descripcion para inactivo"
-            show={show.showTwo}
-            checkedOne={checked.checkedThree}
-            checkedTwo={checked.checkedFour}
-            handleSetShow={() => handleSetShow("showTwo", !show.showTwo)}
-            handleChexboxOne={(e) =>
-              handleSetChecked("checkedThree", e.target.checked)
-            }
-            handleChexboxTwo={(e) =>
-              handleSetChecked("checkedFour", e.target.checked)
-            }
-            handleChexboxOneDelete={(e) =>
-              handleSetDelete("checkedThree", "checkedFour", false, false)
-            }
-          />
-        </div>
-        <div style={{ gridColumn: "4/5" }}>
-          <CardContainer
-            type={"Validación"}
-            titleOne="Sí"
-            titleTwo="No"
-            descriptionOne="Descripcion para sí"
-            descriptionTwo="Descripcion para no"
-            show={show.showThree}
-            checkedOne={checked.checkedFive}
-            checkedTwo={checked.checkedSixx}
-            handleSetShow={() => handleSetShow("showThree", !show.showThree)}
-            handleChexboxOne={(e) =>
-              handleSetChecked("checkedFive", e.target.checked)
-            }
-            handleChexboxTwo={(e) =>
-              handleSetChecked("checkedSixx", e.target.checked)
-            }
-            handleChexboxOneDelete={(e) =>
-              handleSetDelete("checkedFive", "checkedSixx", false, false)
-            }
-          />
-        </div>
-      </div>
-      <ul className={styles.list}>
-        {filterUsers.length === 0
-          ? users.map((user) => <GridItem key={user.id} user={user} />)
-          : filterUsers.map((userFiltered) => <GridItem user={userFiltered} />)}
-      </ul>
-    </section>
+    <div className="conteiner-DashboardAdmin">
+      <h1 className="title-DashboardAdmin">Administradores</h1>
+      <DashboardAdmin>
+        <CardAdmin admins={admins}></CardAdmin>
+      </DashboardAdmin>
+    </div>
   );
 };
