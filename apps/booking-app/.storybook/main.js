cf454111db2b57a8c248781a457af2ab414927cb
Arath Astorga <xarselt@gmail.com>
2022-05-12 15:10:07 -0500

    run global storybook

--- apps/booking-app/.storybook/main.js
+++ apps/booking-app/.storybook/main.js
@@ -1,7 +1,14 @@
 module.exports = {
   "stories": [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+    '../../../packages/authentication/src/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+    '../../../packages/admin-panel/src/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+    '../../../packages/geolocation/src/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+    '../../../packages/messaging-system/src/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+    '../../../packages/places/src/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+    '../../../packages/search-engine/src/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+    '../../../packages/shared/src/**/*.stories.@(js|jsx|ts|tsx|mdx)',
   ],
   "addons": [
     "@storybook/addon-links",
