dbf1df1fa323779bac62d6206bce04a1e2317043
juli666n <julianhfan@gmail.com>
2022-05-12 11:04:38 -0500

    desing added to the favorites list

--- apps/booking-app/pages/favorites/[id]/index.js
+++ apps/booking-app/pages/favorites/[id]/index.js
@@ -14,9 +14,35 @@ export default function Favorite() {
   return (
     <div>
       <Header />
-      <h2 className={styles.paragraph}>{title}</h2>
-      <img src={photoUrl}></img>
-      <p>{favorites}</p>
+      <div className={styles.bodyContainer}>
+        <h2 className={styles.favoriteName}>{title}</h2>
+        <img className={styles.photo} src={photoUrl}></img>
+        <div className={styles.card}>
+          <img className={styles.cardPhoto} src="https://img-co-1.trovit.com/q1OI16cgGE/q1OI16cgGE.1_12.jpg" alt=""></img>
+          <div>
+            <h1 className={styles.cardTitle}>Lujosa suite en ciudad bolivar</h1>
+            <hr></hr>
+            <p>El Apartamento amoblado Ciudad Bolivar ofrece alojamiento con balcón en Ciudad Bolívar. Todos los alojamientos disponen de sala de estar con TV de pantalla plana y cocina con microondas y fogones</p>
+            <div>
+              <p>4.53(15 reseñas)</p>
+              <p>$285,040 COP/noche</p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <img className={styles.cardPhoto} src="https://img-co-1.trovit.com/q1OI16cgGE/q1OI16cgGE.1_12.jpg" alt=""></img>
+          <div>
+            <h1 className={styles.cardTitle}>Lujosa suite en ciudad bolivar</h1>
+            <hr></hr>
+            <p>El Apartamento amoblado Ciudad Bolivar ofrece alojamiento con balcón en Ciudad Bolívar. Todos los alojamientos disponen de sala de estar con TV de pantalla plana y cocina con microondas y fogones</p>
+            <div>
+              <p>4.53(15 reseñas)</p>
+              <p>$285,040 COP/noche</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      
       <Footer />
     </div>
   );}
