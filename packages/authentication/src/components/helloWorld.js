3c63240e9783eef8f736fbde2f4900ffc0b2c6b1
Sebastian.gc <sebastian.gc@globant.com>
2022-05-18 14:57:15 -0500

    Add auth0 login

--- packages/authentication/src/components/helloWorld.js
+++ packages/authentication/src/components/helloWorld.js
@@ -1,23 +1,45 @@
-import { Auth0Provider } from '@auth0/auth0-react';
-import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import React, { useEffect, useState } from 'react';
 
 export const Authentication = () => {
+  const { logout, getAccessTokenSilently, isAuthenticated, loginWithRedirect } =
+    useAuth0();
+
   const handleConectAuthServices = async () => {
-    const response = await fetch('http://localhost:3000/auth/hi');
-    const data = response.json();
-    console.log('>>>data', data);
+    const token = await getAccessTokenSilently({
+      audience: 'https://hinojsa0/api/v2/',
+      scope: 'read:post',
+    });
+    try {
+      const response = await fetch('http://localhost:3001/auth/private', {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const data = await response.json();
+      console.log('>>>data', data);
+    } catch (error) {
+      console.error('Error');
+    }
   };
 
-  return (
-    <Auth0Provider
-      domain="YOUR_DOMAIN"
-      clientId="YOUR_CLIENT_ID"
-      redirectUri="/"
-    >
-      <h1>Hello from Authentication</h1>
-      <button onClick={() => handleConectAuthServices()}>
-        Run service "auth/host"
-      </button>
-    </Auth0Provider>
-  );
+  if (isAuthenticated) {
+    return (
+      <>
+        <h1>HELLO AUTENTICATED</h1>
+        <button onClick={handleConectAuthServices}>Use app</button>
+        <br />
+        <button
+          onClick={() => {
+            logout({ returnTo: window.location.origin });
+          }}
+        >
+          Log out
+        </button>
+      </>
+    );
+  } else {
+    return <button onClick={loginWithRedirect}>Log in</button>;
+  }
 };
