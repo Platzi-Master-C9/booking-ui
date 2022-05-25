41190f752cb3c7efa4af6a40b92798b07b5246a3
CesarGF <cesaringf4@gmail.com>
2022-05-25 16:39:44 -0500

    Style almost finished

--- packages/messaging-system/src/index.js
+++ packages/messaging-system/src/index.js
@@ -1,6 +1,3 @@
 export { MessagingSystem } from './components/helloWorld';
-<<<<<<< HEAD
 export { TextBox } from './components/TextBox/TextBox';
-=======
 export { UserChat } from './components/UserChat/UserChat';
->>>>>>> 84bf487 (workspace ready to work with storybook and create UserChat component)
