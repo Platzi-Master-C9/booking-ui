7a452b1e2c1f422b33f9f40f7d4cec69564c5c0b
Nahomi <84753004+nahyoomi@users.noreply.github.com>
2022-05-30 11:56:21 -0500

    feat: Design view for response time section in chat box #51 (#145)
    * created text chart for messaging system
    
    * export component chart text to the booking-app
    
    * chore: fix typos, export component on storybook and change img for a base64 string
    
    * fixed roots for TextBox
    
    * Created component ResposeTime with modules and SASS
    
    Co-authored-by: Arath Astorga <xarselt@gmail.com>
    Co-authored-by: Arath Astorga <56246139+arselt@users.noreply.github.com>

--- packages/messaging-system/src/index.js
+++ packages/messaging-system/src/index.js
@@ -1,2 +1,3 @@
 export { MessagingSystem } from './components/helloWorld';
 export { TextBox } from './components/TextBox/TextBox';
+export { ResponseTime } from './components/ResponseTime/ResponseTime';
