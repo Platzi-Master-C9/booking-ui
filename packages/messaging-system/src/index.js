0b4a6c07e79b85deb94602fcd7eb9bc7372ad7e5
Arath Astorga <xarselt@gmail.com>
2022-06-05 13:13:21 -0500

    Updated files to jsx and scss, updated export

--- packages/messaging-system/src/index.js
+++ packages/messaging-system/src/index.js
@@ -2,4 +2,4 @@ export { default as MessagingSystem } from './components/helloWorld';
 export { default as TextBox } from './components/TextBox/TextBox';
 export { default as ResponseTime } from './components/ResponseTime/ResponseTime';
 export { default as Card } from './components/card/card';
-export { SideCard } from './components/SideCard/SideCard'
+export { default as SideCard } from './components/SideCard/SideCard'
