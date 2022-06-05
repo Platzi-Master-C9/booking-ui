97589cdc1fe94f502769a8aeb803893ccb67786b
Jose Noriega <josenoriega723@gmail.com>
2022-06-05 15:45:18 -0700

    feat(messages): add chat component to chat page

--- packages/messaging-system/src/pages/Chat/Chat.jsx
+++ packages/messaging-system/src/pages/Chat/Chat.jsx
@@ -1,24 +1,49 @@
+/* eslint-disable no-underscore-dangle */
 // External dependencies
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 // Internal dependencies
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import ChatList from '../../components/ChatList';
+import Chat from '../../components/Chat';
 
 import classes from './Chat.module.scss';
 
 function ChatPage({
   customerId,
 }) {
+  // Component's state
+  const [selectedChat, setSelectedChat] = useState();
+
+  // Handlers
+  const onSelectChat = (chat) => {
+    setSelectedChat(null);
+
+    // We need to until the component is unmounted
+    setTimeout(() => {
+      setSelectedChat(chat);
+    }, 500);
+  };
+
   return (
     <>
       <Header />
       <div className={classes.container}>
         <div className={`${classes.sidePanel} ${classes.chatListContainer}`}>
-          <ChatList userId={customerId} />
+          <ChatList userId={customerId} onSelectChat={onSelectChat} />
         </div>
+        {
+          Boolean(selectedChat) && (
+            <div className={classes.chatContainer}>
+              <Chat
+                customerId={customerId}
+                chatId={selectedChat._id}
+              />
+            </div>
+          )
+        }
       </div>
       <Footer />
     </>
