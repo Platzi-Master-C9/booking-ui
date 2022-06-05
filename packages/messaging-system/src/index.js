5f610b50ca14c59e226530316f155bf050a84636
Arath Astorga <56246139+arselt@users.noreply.github.com>
2022-06-05 13:44:24 -0500

    feat: Create side card linking a chat to a booking post #92 (#158)
    * Created UI for sidecard on messages
    
    * converted styles to module styles
    
    * fix error in ui/side-card
    
    * Updated files to jsx and scss, updated export

--- packages/messaging-system/src/index.js
+++ packages/messaging-system/src/index.js
@@ -2,3 +2,4 @@ export { default as MessagingSystem } from './components/helloWorld';
 export { default as TextBox } from './components/TextBox/TextBox';
 export { default as ResponseTime } from './components/ResponseTime/ResponseTime';
 export { default as Card } from './components/card/card';
+export { default as SideCard } from './components/SideCard/SideCard'
