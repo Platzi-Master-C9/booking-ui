a75085f9b3bba9858950e2cd6df0d5427bc18c68
carlosbritto32 <carlosbritto32@gmail.com>
2022-06-06 20:37:18 -0500

    Feat: adding the layout of the search bar

--- packages/search-engine/src/components/LandingPage/SearchBar/index.jsx
+++ packages/search-engine/src/components/LandingPage/SearchBar/index.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
 import './SearchBar.css';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const SearchBar = () => {
-  return <div className="search__container"></div>;
+  return (
+    <form className="search__container">
+      <div className="search_location">
+        <label>Location</label>
+        <input type="text" placeholder="Where're you going?" />
+      </div>
+      <div className="search_arrivalDepartures">
+        <label>Arrival</label>
+        <input type="text" placeholder="Add dates" />
+      </div>
+      <div className="search_arrivalDepartures">
+        <label>Departure</label>
+        <input type="text" placeholder="Add dates" />
+      </div>
+      <div className="search_Guests">
+        <label>Guests</label>
+        <input type="text" placeholder="How Many?" />
+      </div>
+      <button>
+        <img />
+      </button>
+    </form>
+  );
 };
 
 export default SearchBar;
