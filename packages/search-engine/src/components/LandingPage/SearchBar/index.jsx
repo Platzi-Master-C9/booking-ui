f9d6a445720a035d0ef1a28137e04541a2a66b3e
carlosbritto32 <carlosbritto32@gmail.com>
2022-06-07 10:46:48 -0500

    feat:adding functionality to the date picker of the search bar, now you can select the date range!

--- packages/search-engine/src/components/LandingPage/SearchBar/index.jsx
+++ packages/search-engine/src/components/LandingPage/SearchBar/index.jsx
@@ -1,30 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Link from 'next/link';
 import './SearchBar.css';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
 const SearchBar = () => {
+  const [showMenu, setShowMenu] = useState(null);
+
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+
+  const toggleMenu = (e) => {
+    !showMenu ? setShowMenu('show_menu-guest') : setShowMenu(null);
+    e.stopPropagation();
+  };
+
   return (
     <form className="search__container">
       <div className="search_location">
         <label>Location</label>
         <input type="text" placeholder="Where're you going?" />
       </div>
-      <div className="search_arrivalDepartures">
+      <div className="search_location search_arrivalDepartures">
         <label>Arrival</label>
-        <input type="text" placeholder="Add dates" />
+        <DatePicker
+          className="picker_box"
+          placeholderText="Add dates"
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+          selectsStart
+          startDate={startDate}
+          endDate={endDate}
+          minDate={new Date()}
+          dateFormat="dd/MM/yyyy"
+        />
       </div>
-      <div className="search_arrivalDepartures">
+      <div className="search_location search_arrivalDepartures">
         <label>Departure</label>
-        <input type="text" placeholder="Add dates" />
+        <DatePicker
+          className="picker_box"
+          placeholderText="Add dates"
+          selected={endDate}
+          onChange={(date) => setEndDate(date)}
+          selectsEnd
+          startDate={startDate}
+          endDate={endDate}
+          minDate={startDate}
+          dateFormat="dd/MM/yyyy"
+        />
       </div>
-      <div className="search_Guests">
+      <div className="search_location search_Guests">
         <label>Guests</label>
-        <input type="text" placeholder="How Many?" />
+        <div className="input-disable" onClick={toggleMenu}>
+          How Many?
+        </div>
+        <div className={`menu_guest ${showMenu}`}>
+          <div className="menu-options">
+            <p>Adults</p>
+            <input type="number" min={0} max={9} />
+          </div>
+          <div className="menu-options">
+            <p>Children</p>
+            <input type="number" min={0} max={9} />
+          </div>
+          <div className="menu-options">
+            <p>Pets</p>
+            <input type="number" min={0} max={9} />
+          </div>
+        </div>
       </div>
-      <button>
-        <img />
-      </button>
+      <div className="icon-search">
+        <Link href="/search">
+          <a className="icon-png"></a>
+        </Link>
+      </div>
     </form>
   );
 };
