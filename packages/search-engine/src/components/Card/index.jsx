d7a84dd2226dd4c177183fe3221f53203025e193
Simon Torres <simontorres979797@gmail.com>
2022-06-06 20:31:13 -0500

    integrate component card with search page results

--- packages/search-engine/src/components/Card/index.jsx
+++ packages/search-engine/src/components/Card/index.jsx
@@ -1,31 +1,32 @@
 import React from 'react';
 import './Card.css';
 
-const CardComponent = () => {
+const CardComponent = (props) => {
+  const { subtitle, title, description, rating, price, reviews, image } = props
   return (
     <div className='wrap-card'>
       <div className='wrap-slider'>
         <div className='slider-arrow__right'></div>
-        <img src='../../assets/images/card-img.png' alt='' />
+        <img src={image} alt='' />
       </div>
       <div className='card-content'>
-        <img className='card-content__fav' src='../../assets/icons/icon-heart.png' alt='' />
-        <span className='card-content__subtitle'>Loft entero en Medellín</span>
-        <h1 className='card-content__title'>New Loft near Parque de los Pies De...</h1>
+        <img className='card-content__fav' src='/search-engine/icon-heart.png' alt='' />
+        <span className='card-content__subtitle'>{subtitle}</span>
+        <h1 className='card-content__title'>{title}</h1>
         <p className='card-content__description'>
-          2 huéspedes · 1 habitación · 1 cama · 1 baño · Wifi · Cocina · Estacionamiento gratuito
+          {description}
         </p>
         <div className='card-content__footer'>
           <div className='card-content__reviews'>
             <figure>
-              <img src='../../assets/icons/arrow-right.png' alt='' />
-              <figcaption>
-                <strong>4,25</strong><span>(15 reseñas)</span>
+              <img src='/search-engine/icon-star.png' alt='' />
+              <figcaption className='card-content__reviews_text'>
+                <strong>{rating}</strong><span>({reviews} reseñas)</span>
               </figcaption>
             </figure>
           </div>
           <div className='card-content__price'>
-            <span>$285,040 </span>COP / noche
+            <span>${price} </span>COP / noche
           </div>
         </div>
       </div>
--- packages/search-engine/src/components/resultsPage/ListResults.jsx
+++ packages/search-engine/src/components/resultsPage/ListResults.jsx
@@ -4,6 +4,7 @@ import './styles/ListResults.css';
 import './styles/loader.css';
 
 function ListResults({ places, statePlaces }) {
+  console.log(statePlaces, 'statePlaces');
   return <React.Fragment>
     <section className='list__results'>
       { statePlaces.loading &&
@@ -15,12 +16,22 @@ function ListResults({ places, statePlaces }) {
         <h1>An error has occurred: <span>{statePlaces.error}</span></h1>
       }
       { (!statePlaces.loading && !places.length) &&
-        // <h1>Places not found</h1>
-        <CardComponent />
+        <h1>Places not found</h1>
       }
-      { places.map(card => {
-        return <div className="results__card" key={card.uuid}> {card.city} </div>
-      })}
+      {places.map((place) => {
+        return (
+          <CardComponent 
+            key={place.id}
+            subtitle={place.title}
+            title={place.type}
+            description={place.perks}
+            rating={place.rating}
+            price={place.price}
+            reviews={place.reviews}
+            image={place.images[0]}
+          />
+        )
+      })};
     </section>
   </React.Fragment>
 }
