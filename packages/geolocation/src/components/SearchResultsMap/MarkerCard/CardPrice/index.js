61cf0353fa02d06b057d757ddc495fddabfe1afd
Emanuel Alvarado <emanuelalvaradog@gmail.com>
2022-06-07 18:10:04 -0500

    chore: Integrate search results map to results page

--- packages/geolocation/src/components/SearchResultsMap/MarkerCard/CardPrice/index.js
+++ packages/geolocation/src/components/SearchResultsMap/MarkerCard/CardPrice/index.js
@@ -4,7 +4,7 @@ import styles from './cardPrice.module.css';
 function CardPrice({ price }) {
   return (
     <div className={styles.price}>
-      <p>${price} MXN</p>
+      <p>${price} COP</p>
       <p>/ noche</p>
     </div>
   );
